e2cd99e8d5a755b573e296b394fc153241b8a75f
Merradou-Abderrahmane <abderahmane.merradou@gmail.com>
2022-03-29 11:08:54 +0000

    get all products method

--- prototypes/prototype-2/productsManager.php
+++ prototypes/prototype-2/productsManager.php
@@ -2,7 +2,7 @@
 require_once "product.php";
 
 class ProductManager {
-
+    // connect to DB method + check for possible errors
     private $connection = NULL;
 
     public function connectToDB(){
@@ -16,6 +16,25 @@ class ProductManager {
             }
             return $this->connection;
         }
+    }
+    
+    // Display all products method ( in index.php)
+
+    public function getAllProducts(){
+        $selectedProducts = 'SELECT * FROM prototype_2';
+        $query = mysqli_query($this->connectToDB(), $selectedProducts);
+        $result = mysqli_fetch_all($query, MYSQLI_ASSOC);
+
+        $productsArray = array();
+        foreach($result as $resultValue){
+            $product = new Product();
+            $product->setId($resultValue["id"]);
+            $product->setName($resultValue["name"]);
+            $product->setPrice($resultValue["price"]);
+
+            array_push($productsArray, $product);
+        }
+        return $productsArray;
 
 
 
