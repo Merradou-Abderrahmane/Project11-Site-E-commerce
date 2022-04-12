f26ca117b2b9e89f51dedc6739c06b6eface22a9
Merradou-Abderrahmane <abderahmane.merradou@gmail.com>
2022-04-12 10:33:13 +0000

    save

--- src/admin/includes/templates/footer.php
+++ src/admin/includes/templates/footer.php
@@ -2,7 +2,7 @@
         <div class="footer">
             this is a footer
         </div>
-        <script src="layout/js/jQuery.js" ></script>  
+        <script src="layout/js/jQuery.js" ></script>  <!-- jQuery -->
         <script src="layout/js/bootstrap.min.js" ></script>  <!-- bootstrap js -->
 </body>
 </html>
--- src/admin/includes/templates/header.php
+++ src/admin/includes/templates/header.php
@@ -6,12 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>admin</title>
     <link rel="stylesheet" href="/layout/css/bootstrap.min.css">
-    <link rel="stylesheet" href="layout/css/admin.css">
+    <link rel="stylesheet" href="/layout/css/fontawesome.min.css">
+    <link rel="stylesheet" href="/layout/css/admin.css">
 </head>
 <body>
-    
-
-    
-</body>
-</html>
-    
