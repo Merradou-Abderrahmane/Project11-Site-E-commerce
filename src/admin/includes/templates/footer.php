6ff580436184cf45e210963ed5db24b41de1930e
Merradou-Abderrahmane <abderahmane.merradou@gmail.com>
2022-04-12 10:24:42 +0000

    initialize

---
+++ src/admin/includes/templates/footer.php
@@ -0,0 +1,8 @@
+ 
+        <div class="footer">
+            this is a footer
+        </div>
+        <script src="layout/js/jQuery.js" ></script>  
+        <script src="layout/js/bootstrap.min.js" ></script>  <!-- bootstrap js -->
+</body>
+</html>
---
+++ src/admin/includes/templates/index.php
@@ -0,0 +1,10 @@
+<?php
+
+include "header.php";
+?>
+  <button type="button" class="btn btn-primary">Primary</button>
+   welcome to index page 
+<?php
+
+include "footer.php";
+?>
